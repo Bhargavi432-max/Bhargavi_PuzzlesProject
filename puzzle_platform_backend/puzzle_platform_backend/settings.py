7f7a13b3809d2d8359a582c89195126557db7682
uday80022 <uday80022@gmail.com>
2024-02-12 12:00:57 +0530

    Added backend and database

--- puzzle_platform_backend/puzzle_platform_backend/settings.py
+++ puzzle_platform_backend/puzzle_platform_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'puzzle_database',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'puzzle_platform_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'video_puzzle_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
