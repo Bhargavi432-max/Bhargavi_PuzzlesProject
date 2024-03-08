3e0a9d8cc55d9e73f111ebbc4c9a75202cd8e5d2
YASHASVINI-THANNIRU <s180493@rguktsklm.ac.in>
2024-03-08 19:31:05 +0530

    added puzzle page message

--- puzzle_platform_backend/puzzle_frontend/src/components/PuzzlePage.js
+++ puzzle_platform_backend/puzzle_frontend/src/components/PuzzlePage.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import SideNavbar from "./SideNavbar";
 import Content from "./Content";
 import { useNavigate } from "react-router-dom";
-//import { useEmail } from "./EmailContext";
+import WelcomImage from "./Images/WelcomImage.png";
+import CloudImage from"./Images/CloudImage.png";
 import "./PuzzlePage.css";
 
 const PuzzlePage = () => {
@@ -14,6 +15,7 @@ const PuzzlePage = () => {
   const [authenticated, setAuthenticated] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
   const [puzzleData, setPuzzleData] = useState([null]);
+  const [showWelcomeMessage, setShowWelcomeMessage] = useState(true); // State to control welcome message visibility
   const navigate = useNavigate();
   const email = localStorage.getItem('email');
   useEffect(() => {
@@ -25,7 +27,10 @@ const PuzzlePage = () => {
     }
   }, [email, navigate]);
 
+  const welcomeMessage = "Welcome to the puzzle page. Let's begin!";
+
   const handleSidebarButtonClick = async (id) => {
+    setShowWelcomeMessage(false); // Hide welcome message when a task is clicked
     try {
       const response = await fetch("http://127.0.0.1:8000/api/get_ids/", {
         method: "POST",
@@ -47,10 +52,6 @@ const PuzzlePage = () => {
     }
   };
 
-  // useEffect(() => {
-  //   handleSidebarButtonClick(); 
-  // }, [email]);
-
   return (
     authenticated && (
       <div className="container-fluid">
@@ -63,10 +64,18 @@ const PuzzlePage = () => {
             />
           </div>
           <div className="content-box">
-            <Content
-              selectedTask={selectedTask}
-              puzzleData={puzzleData} 
-            />
+            {showWelcomeMessage ? ( // Display welcome message conditionally
+              <div className={`cloud-message-container ${showWelcomeMessage ? 'show-welcome-message' : ''}`}>
+                <div className="welcome-message">{welcomeMessage}</div>
+                {/* <img src={CloudImage} alt="cloud Image" /> */}
+                <img src={WelcomImage} alt="Welcome Image" className="welcome-image"/>
+              </div>
+            ) : (
+              <Content
+                selectedTask={selectedTask}
+                puzzleData={puzzleData}
+              />
+            )}
           </div>
         </div>
       </div>
