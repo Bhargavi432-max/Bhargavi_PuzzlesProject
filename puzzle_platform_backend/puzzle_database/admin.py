26c4aee8d7323401c3876fb018c939669945aa37
finalproject565 <finalproject565@gmail.com>
2024-03-08 18:13:31 +0530

    Fixed errors

--- puzzle_platform_backend/puzzle_database/admin.py
+++ puzzle_platform_backend/puzzle_database/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Admin, DataTable, UserProfile, Subscription, Report, Plan, UserDataTableStatus
+from .models import CustomUser, Admin, DataTable, UserProfile, Subscription, LogReport, Plan, UserDataTableStatus
 
 
 admin.site.register(CustomUser)
@@ -7,7 +7,7 @@ admin.site.register(Admin)
 admin.site.register(DataTable)
 admin.site.register(UserProfile)
 admin.site.register(Subscription)
-admin.site.register(Report)
+admin.site.register(LogReport)
 admin.site.register(Plan)
 admin.site.register(UserDataTableStatus)
 
