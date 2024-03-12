07b09040f94ab9fd695e4a585ee39d1a782bb9ff
YASHASVINI-THANNIRU <s180493@rguktsklm.ac.in>
2024-03-12 20:04:34 +0530

    added contactus page details to backend

--- puzzle_platform_backend/puzzle_frontend/src/components/ContactUsPage.js
+++ puzzle_platform_backend/puzzle_frontend/src/components/ContactUsPage.js
@@ -8,6 +8,8 @@ function ContactUsPage() {
     phoneNumber: ''
   });
 
+  const [submissionStatus, setSubmissionStatus] = useState(null);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -19,7 +21,7 @@ function ContactUsPage() {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Send form data to backend endpoint
-    fetch('backend_endpoint_url', {
+    fetch('http://127.0.0.1:8000/api/contact_us/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -35,9 +37,13 @@ function ContactUsPage() {
         email: '',
         phoneNumber: ''
       });
+      // Set submission status
+      setSubmissionStatus('success');
     })
     .catch(error => {
       console.error('Error submitting form:', error);
+      // Set submission status
+      setSubmissionStatus('error');
     });
   };
 
@@ -45,45 +51,49 @@ function ContactUsPage() {
     <div className='contact-main'>
       <div className='text-contact'>If you need personalized assistance or have specific inquiries, feel free to reach out to our support team. You can contact us via email at support@example.com or by filling out the contact form below.</div>
       <div className="contact-container">
-        <form className="contact-form" onSubmit={handleSubmit}>
-          <div className="input-container">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              placeholder='users name'
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              placeholder='user@gmail.com'
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="phoneNumber">Phone Number</label>
-            <input
-              type="tel"
-              id="phoneNumber"
-              name="phoneNumber"
-              value={formData.phoneNumber}
-              placeholder='mobile number'
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <button type="submit">Submit</button>
-        </form>
+        {submissionStatus === 'success' ? (
+          <p className="submission-message">Successfully submitted. Check your email.</p>
+        ) : (
+          <form className="contact-form" onSubmit={handleSubmit}>
+            <div className="input-container">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                placeholder='users name'
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="input-container">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                placeholder='user@gmail.com'
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="input-container">
+              <label htmlFor="phoneNumber">Phone Number</label>
+              <input
+                type="tel"
+                id="phoneNumber"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                placeholder='mobile number'
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        )}
       </div>
     </div>
   );
