ebbdb7ad7efca1037f933f48da864fb15ffa2f79
unknown <uday80022@gmail.com>
2024-03-14 19:44:47 +0530

    Fixed bug

--- puzzle_platform_backend/puzzle_frontend/src/components/Content.js
+++ puzzle_platform_backend/puzzle_frontend/src/components/Content.js
@@ -5,7 +5,6 @@ import Wallet from "./SubscriptionPage";
 import { useNavigate } from "react-router-dom";
 
 const Content = ({ selectedTask, puzzleData }) => {
-  const [Isvideocomplete, setIsvideocomplete] = useState(false);
   const [selectedPuzzle, setSelectedPuzzle] = useState(null);
   const navigate = useNavigate();
   const [selectedPuzzleDetails, setSelectedPuzzleDetails] = useState(null);
@@ -82,7 +81,6 @@ const Content = ({ selectedTask, puzzleData }) => {
   const handleDifficultyBoxButtonClick = (puzzleId) => {
     const clickedPuzzle = puzzleData.find((puzzle) => puzzle.puzzle_id === puzzleId);
     console.log("click:",clickedPuzzle.puzzle_locked);
-    
     if(!clickedPuzzle.puzzle_locked){
     setSelectedPuzzle(clickedPuzzle);
     
@@ -130,7 +128,6 @@ const Content = ({ selectedTask, puzzleData }) => {
   };
 
   const handleVideoStart = () => {
-    setIsvideocomplete(false);
     const currentDateTime = new Date();
     const startTime = currentDateTime.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'}) + ' ' + currentDateTime.toLocaleTimeString('en-US', {hour12: false}); 
     setStartTime(startTime);
@@ -155,7 +152,6 @@ const Content = ({ selectedTask, puzzleData }) => {
     const taskStatus = "incomplete";
     const actionItem = "puzzle completed";
     setCompletedPuzzles([...completedPuzzles, selectedPuzzle.puzzle_id]);
-    setIsvideocomplete(true);
 
     // Fetch request to mark puzzle status
     fetch("http://127.0.0.1:8000/api/mark_puzzle_status/", {
