6bb972f378701db72185119611186557644c6d98
Bhargavi-max <bhargavithanniru432@gmail.com>
2024-04-03 12:23:01 +0530

    added billing

--- puzzle_platform_backend/puzzle_frontend/src/components/BillingAndTaxPage.js
+++ puzzle_platform_backend/puzzle_frontend/src/components/BillingAndTaxPage.js
@@ -1,11 +1,35 @@
 // BillingAndTaxPage.js
-import React from 'react';
+import React ,{useEffect} from 'react';
 
 const BillingAndTaxPage = () => {
+  useEffect(() => {
+    // Fetch user information from the backend when the component mounts
+    fetchbilldetails();
+  }, []);
+
+  const fetchbilldetails = async () => {
+    const email = localStorage.getItem('email');
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/get_payment_history/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email })
+      });
+      const data = await response.json();
+      if (data.success) {
+        console.log({data});
+      } else {
+        console.error('Failed to fetch user information:', data.error);
+      }
+    } catch (error) {
+      console.error('Error fetching user information:', error);
+    }
+  };
   return (
     <div>
       <h2>Billing and Tax Page</h2>
-      {/* Your billing and tax settings form/components */}
     </div>
   );
 };
