3d1e85e6f462d11dbccd725e10e53f0198b15d1e
Bhargavi-max <bhargavithanniru432@gmail.com>
2024-04-12 17:29:47 +0530

    app.js

--- puzzle_platform_backend/puzzle_frontend/src/App.js
+++ puzzle_platform_backend/puzzle_frontend/src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/JSFiles/Navbar";
 import './App.css';
-import { Login } from "./components/Login";
-import { Register } from "./components/Register";
-import OTPVerification from "./components/OTPVerification";
-import ChangePassword from "./components/ChangePassword";
-import ForgotPasswordPage from "./components/ForgotPasswordPage";
-import CheckOTPPage from "./components/CheckOTPPage";
-import ResetPasswordPage from "./components/ResetPasswordPage";
-import PuzzlePage from "./components/PuzzlePage";
-import { EmailProvider } from "./components/EmailContext";
-import HomePage from "./components/HomePage";
-import PaymentFailPage from "./components/PaymentFailPage";
-import PaymentSuccessPage from "./components/PaymentSuccessPage";
-import TwoStepOtp from "./components/TwoStepOtp";
+import { Login } from "./components/JSFiles/Login";
+import { Register } from "./components/JSFiles/Register";
+import OTPVerification from "./components/JSFiles/OTPVerification";
+import ChangePassword from "./components/JSFiles/ChangePassword";
+import ForgotPasswordPage from "./components/JSFiles/ForgotPasswordPage";
+import CheckOTPPage from "./components/JSFiles/CheckOTPPage";
+import ResetPasswordPage from "./components/JSFiles/ResetPasswordPage";
+import PuzzlePage from "./components/JSFiles/PuzzlePage";
+import { EmailProvider } from "./components/JSFiles/EmailContext";
+import HomePage from "./components/JSFiles/HomePage";
+import PaymentFailPage from "./components/JSFiles/PaymentFailPage";
+import PaymentSuccessPage from "./components/JSFiles/PaymentSuccessPage";
+import TwoStepOtp from "./components/JSFiles/TwoStepOtp";
 
 function App() {
   return (
